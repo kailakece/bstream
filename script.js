59c1b51758e1e3cf5e9ee523bedd0861e61553ff
kailakece <kailapirdaaulia@gmail.com>
2026-06-04 13:01:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const WORKER_BASE_URL = "https://bstream.kailapirdaaulia.workers.dev/";
+const WORKER_BASE_URL = "https://play.kailapirdaaulia.workers.dev/";
 const TEXT_PLAYLIST_URL = "./playlist.txt"; 
 
 let databaseVideo = {}; 
